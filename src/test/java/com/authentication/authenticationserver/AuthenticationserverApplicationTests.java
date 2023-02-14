5b66ca44b56b9600e2cba16771e24cc9bc62fa09
mourya-satyam-888 <satyammourya@Administrators-MacBook-Pro.local>
2023-02-14 17:00:14 +0530

    initia setup

---
+++ src/test/java/com/authentication/authenticationserver/AuthenticationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.authenticationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
